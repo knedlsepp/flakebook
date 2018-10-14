9ea5564257e50b676782875e36e4a880a6cba16b
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-10-14 10:41:03 +0200

    Enable fuzzy bash completions
    Usage:
    
    COMMAND [DIRECTORY/][FUZZY_PATTERN]**<TAB>

--- configuration.nix
+++ configuration.nix
@@ -181,6 +181,7 @@
     interactiveShellInit = ''
       if command -v fzf-share >/dev/null; then
         source "$(fzf-share)/key-bindings.bash"
+        source "$(fzf-share)/completion.bash"
       fi
     '';
   };
