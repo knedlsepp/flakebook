b66f7e17ea6ec9474b947ed53583e168983ef521
Josef Kemetmueller <josef,kemetmueller@gmail.com>
2019-07-16 18:06:36 +0000

    Try enable intel video driver
    intel_gpu_top shows no usage of the video acceleration
    capabilities, which this commit tries to solve

--- configuration.nix
+++ configuration.nix
@@ -70,6 +70,7 @@ in
   };
   nixpkgs.overlays = [
     (self: super: {
+      vaapiIntel = super.vaapiIntel.override { enableHybridCodec = true; };
       myVim = super.vim_configurable.customize {
         name = "vi"; # The name is used as a binary!
         vimrcConfig = {
@@ -655,9 +656,19 @@ in
   hardware.enableAllFirmware = true;
   hardware.cpu.intel.updateMicrocode = true;
   hardware.sensor.iio.enable = true;
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-media-driver
+    ];
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "intel" "ati" "cirrus" "vesa" "vmware" "modesetting" ];
   services.xserver.layout = "us,de";
   services.xserver.xkbOptions = "eurosign:e";
 
