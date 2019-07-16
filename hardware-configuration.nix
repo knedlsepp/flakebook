22590988f8435cab151ee7cbef4794aeefc149df
Josef Kemetmueller <josef,kemetmueller@gmail.com>
2019-07-16 20:56:38 +0200

    Re-run nixos-generate-config

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -9,6 +9,7 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
